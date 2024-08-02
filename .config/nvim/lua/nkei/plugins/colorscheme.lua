b6f35add6685627f6af9304ac51122f8c8da7f18
nkei <keitaro.noda@gmail.com>
2024-08-02 21:35:12 +0900

    -

--- .config/nvim/lua/nkei/plugins/colorscheme.lua
+++ .config/nvim/lua/nkei/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    "catppuccin/nvim",
-    priority = 1500,
-    config = function()
-        require("catppuccin").setup({
-            flavour = "frappe",
-        })
-    vim.cmd.colorscheme("catppuccin-mocha")
-    vim.cmd("highlight TelescopeSelection cterm=bold gui=bold guifg=#a6e3a1 guibg=#181825")
-    end,
+  "catppuccin/nvim",
+  priority = 1500,
+  config = function()
+      require("catppuccin").setup({
+          flavour = "frappe",
+      })
+  vim.cmd.colorscheme("catppuccin-mocha")
+  vim.cmd("highlight TelescopeSelection cterm=bold gui=bold guifg=#a6e3a1 guibg=#181825")
+  end,
 }
 
